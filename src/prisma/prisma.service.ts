b7f54b7e9cfc83aefad7ebd1abd1d61b756aba21
Danylo Zalizchuk <danilmail0110@gmail.com>
2023-05-10 21:59:39 +0300

    feat: add db models and connect to db

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,19 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({ log: ['query', 'info', 'warn', 'error'] });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', () => {
+      app.close();
+    });
+  }
+}
