bf8a23b06f5f0149d01e7599279d679ea61f40b2
Danylo Zalizchuk <danilmail0110@gmail.com>
2023-05-15 20:54:20 +0300

    feat: delete unused files and inactive products

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SchedulerService } from './scheduler.service';
+import { FileModule } from 'src/file/file.module';
 
 // Scheduler Module
 @Module({
+  imports: [FileModule],
   providers: [SchedulerService],
 })
 export class SchedulerModule {}
--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -1,10 +1,40 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { FileService } from 'src/file/file.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SchedulerService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private fileService: FileService,
+  ) {}
 
   private readonly logger = new Logger(SchedulerService.name);
+
+  @Cron(CronExpression.EVERY_WEEK)
+  async deleteUnusedFiles() {
+    const files = await this.prismaService.image.findMany({
+      where: { isActive: false },
+      select: { id: true, url: true },
+    });
+
+    // Delete files from the file system and from the database
+    const deletionPromises = [
+      ...files.map((item) => this.fileService.deleteFile(item.url)),
+      this.prismaService.image.deleteMany({ where: { isActive: false } }),
+    ];
+
+    await Promise.all(deletionPromises);
+    this.logger.debug('All unused files has been deleted');
+  }
+
+  @Cron(CronExpression.EVERY_WEEK)
+  async deleteInactiveProducts() {
+    await this.prismaService.product.deleteMany({
+      where: { status: 'INACTIVE' },
+    });
+
+    this.logger.debug('All inactive products has been deleted');
+  }
 }
