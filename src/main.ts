ddaa0bc490c082f96f79241a62e7067adb74ea35
Danylo Zalizchuk <danilmail0110@gmail.com>
2023-05-16 21:16:51 +0300

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api');
-  app.enableCors();
+  app.enableCors({ origin: process.env.CLIENT_BASE_URL, credentials: true });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
